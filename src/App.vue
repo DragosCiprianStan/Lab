0bd729c13a0a7c8568e9e1f667dd57f4aa7186a5
DragosCiprianStan <108654619+DragosCiprianStan@users.noreply.github.com>
2022-11-23 20:24:04 +0200

    Root betwen page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,50 @@
-<script></script>
+
 <script setup>
 import Api from "./components/icons/Api.vue";
-</script>
+import Home from "./components/icons/Home.vue";
 
+import { useTheme } from "vuetify";
+</script>
+<script>
+const routes = {
+  "/": Home,
+  "/Api": Api,
+};
+export default {
+  setup() {
+    const theme = useTheme();
+
+  },
+  data() {
+    return {
+      currentPath: window.location.hash,
+    };
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || "/"] || NotFound;
+    },
+  },
+  mounted() {
+    window.addEventListener("hashchange", () => {
+      this.currentPath = window.location.hash;
+    });
+  },
+  methods: {},
+};
+</script>
 <template>
 
-<main>
-    <Api />
+  <main>
+    <v-app>
+      <v-app-bar title="My Deck">
+        <v-btn href="#/" icon="mdi-home"></v-btn>
+        <v-btn href="#/Api" icon="mdi-cards"></v-btn>
+      </v-app-bar>
+      <v-main>
+        <component :is="currentView" />
+      </v-main>
+    </v-app>
   </main>
 </template>
 
